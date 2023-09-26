565799f69636b3aaefa91557fd41223bb63c14be
Douglas Marinho Silva <douglas.dsn.in@alterdata.com.br>
2023-09-26 08:48:35 -0300

    Desafio FullCycle

---
+++ nodejs/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config ={
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'fullcycledb'
+};
+const mysql = require('mysql')
+const connection= mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values ('Douglas')`
+connection.query(sql)
+connection.end()
+
+app.get('/', (req, res)=>{
+    res.send('<h1>Full Cycle Rocks!</h1>')
+})
+
+app.listen(port, ()=>{
+    console.log('Rodando na porta ' + port)
+})
